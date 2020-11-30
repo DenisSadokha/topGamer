e6a0087f7ef70afc5f495d135141a42fdc92dfd9
DenisSadokha <71551573+DenisSadokha@users.noreply.github.com>
2020-11-30 12:40:55 +0700

    Update FuckOff.java

--- app/src/main/java/com/example/realmlist/FuckOff.java
+++ app/src/main/java/com/example/realmlist/FuckOff.java
@@ -7,6 +7,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FuckOff extends AppCompatActivity {
+    Button b;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
